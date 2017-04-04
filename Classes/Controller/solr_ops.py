c5382dbda14398d7723ab2e16dd4a8e25207a658
pdavis <pdavis@nationalarchives.gov.uk>
2017-04-04 11:13:16 +0100

    amended functions for finding tagged and non tagged documents. Added functions for A2A data set

--- Classes/Controller/solr_ops.py
+++ Classes/Controller/solr_ops.py
@@ -10,6 +10,8 @@ class SolrOperations:
         self.tx_id = ''
         self.iaid = iaid
         self.q = ''
+        self.src_tna = 'SOURCE:100 OR SOURCE:200 AND '
+        self.src_a2a = 'SOURCE:600 AND '
         self.con = connection
 
     def add_taxonomy(self):
@@ -22,20 +24,42 @@ class SolrOperations:
         for doc in docs:
             self.con.add([doc], fieldUpdates={'TAXONOMY': 'remove', 'TAXONOMYID': 'remove'})
 
-# Search query to find results where document is not tagged + matches query
-    def find_not_tagged(self):
-        results = self.con.search('DOCREFERENCE:' + self.iaid + ' AND ' + self.q + ' NOT TAXONOMYID:' + self.tx_id)
+# Search query to find results where document is not tagged + matches query (tna)
+    def find_not_tagged_tna(self):
+        results = self.con.search(self.src_tna + 'DOCREFERENCE:' + self.iaid + ' AND ' + self.q + ' NOT TAXONOMYID:'
+                                  + self.tx_id)
         print("Docs Found, not tagged {0} Result(s).".format(len(results)))
         for result in results:
             print("IAID: '{0}'.".format(result['DOCREFERENCE']))
             return self.iaid
-    find_not_tagged()
+    find_not_tagged_tna()
 
-# search query to find results where document is tagged + no longer matches query
-    def find_is_tagged(self):
-        results = self.con.search('DOCREFERENCE:' + self.iaid + ' AND TAXONOMYID:' + self.tx_id + ' NOT ' + self.q)
+# search query to find results where document is tagged + no longer matches query (tna)
+    def find_is_tagged_tna(self):
+        results = self.con.search(self.src_tna + 'DOCREFERENCE:' + self.iaid + ' AND TAXONOMYID:' + self.tx_id + ' NOT '
+                                  + self.q)
         print("Docs Found, tagged {0} Result(s).".format(len(results)))
         for result in results:
             print("IAID: '{0}'.".format(result['DOCREFERENCE']))
             return self.iaid
-    find_is_tagged()
+    find_is_tagged_tna()
+
+# Search query to find results where document is not tagged + matches query (a2a)
+    def find_not_tagged_a2a(self):
+        results = self.con.search(self.src_a2a + 'DOCREFERENCE:' + self.iaid + ' AND ' + self.q + ' NOT TAXONOMYID:'
+                                  + self.tx_id)
+        print("Docs Found, not tagged {0} Result(s).".format(len(results)))
+        for result in results:
+            print("IAID: '{0}'.".format(result['DOCREFERENCE']))
+            return self.iaid
+    find_not_tagged_a2a()
+
+# search query to find results where document is tagged + no longer matches query (a2a)
+    def find_is_tagged_a2a(self):
+        results = self.con.search(self.src_a2a + 'DOCREFERENCE:' + self.iaid + ' AND TAXONOMYID:' + self.tx_id + ' NOT '
+                                  + self.q)
+        print("Docs Found, tagged {0} Result(s).".format(len(results)))
+        for result in results:
+            print("IAID: '{0}'.".format(result['DOCREFERENCE']))
+            return self.iaid
+    find_is_tagged_a2a()

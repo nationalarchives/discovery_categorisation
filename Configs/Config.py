125d93f36267e0765ef287e2a9cfbddd44bec875
pdavis <pdavis@nationalarchives.gov.uk>
2017-04-04 13:39:07 +0100

    edited config settings

--- Configs/Config.py
+++ Configs/Config.py
@@ -1,11 +1,11 @@
 class Config:
     """Class to contain project level constants and attributes
     Should be imported in to every module"""
-    MONGODB_SERVER="wb-solrnutch1.web.local"
-    MONGODB_PORT="27017"
-    MONGODB_DATABASE="taxonomy"
-    MONGODB_COLLECTION="categories"
+    MONGODB_SERVER = "wb-solrnutch1.web.local"
+    MONGODB_PORT = "27017"
+    MONGODB_DATABASE = "taxonomy"
+    MONGODB_COLLECTION = "categories"
 
-    SOLR_SERVER="wb-solrnutch1.web.local"
-    SOLR_PORT="8986"
-    SOLR_CORE="DISCOVERY"
+    SOLR_SERVER = "wb-solrnutch1.web.local"
+    SOLR_PORT = "8986"
+    SOLR_CORE = "DISCOVERY"
